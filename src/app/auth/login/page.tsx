7eec69c17b6fc033de0c1c5bbeec3b4fc509767e
buky <alao@connou.app>
2026-02-21 13:17:28 +0100

    project setup with layouts font family

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,11 @@
+import styles from './login.module.scss'
+
+export default function LoginPage() {
+  return (
+    <div className={styles.container} >
+      <div >
+        Login Form
+      </div>
+    </div>
+  )
+}
