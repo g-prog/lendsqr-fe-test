340f2e1eb7b9e830f31b52fa55bf100114e99047
buky <alao@connou.app>
2026-02-22 12:41:23 +0100

    starting out with dashboard layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,11 +1,24 @@
+import layoutStyles from "./layout.module.scss";
+import Image from "next/image";
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <div>
-      {children}
+    <div className={layoutStyles.wrapper}>
+      <div className={layoutStyles.navBar}>
+        <div className={layoutStyles.logoContainer}>
+          <Image src="/logo.svg" fill alt="logo" />
+        </div>
+      </div>
+      <div className={layoutStyles.container}>
+        <div className={layoutStyles.layoutLeft}>
+          <p>left</p>
+        </div>
+
+        <div className={layoutStyles.layoutRight}>{children}</div>
+      </div>
     </div>
   );
 }
