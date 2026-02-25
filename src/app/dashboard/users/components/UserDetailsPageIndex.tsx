a80fcee131e174bc05c91b623ed3d8d9fdd7cf62
Dantown <dantown@Dantowns-MacBook-Air.local>
2026-02-25 12:48:30 +0100

    almost done with user details top section

--- src/app/dashboard/users/components/UserDetailsPageIndex.tsx
+++ src/app/dashboard/users/components/UserDetailsPageIndex.tsx
@@ -1,6 +1,8 @@
 import detailsStyles from "../userdetails.module.scss";
 import LongForwardIcon from "@/components/icons/LongForwardIcon";
 import SvgAvatar from "@/components/icons/SvgAvatar";
+import FullStar from "@/components/icons/FullStar";
+import EmptyStar from "@/components/icons/EmptyStar";
 
 const UserDetailsPageIndex = () => {
   return (
@@ -22,8 +24,33 @@ const UserDetailsPageIndex = () => {
       </div>
 
       <div className={detailsStyles.generalInfoDiv}>
-        <div className={detailsStyles.iconContainer}>
-          <SvgAvatar />
+        <div className={detailsStyles.infoWrapper}>
+          <div className={detailsStyles.nameFlex}>
+            <div className={detailsStyles.iconContainer}>
+              <SvgAvatar />
+            </div>
+
+            <div className={detailsStyles.nameCol}>
+              <h4>Grace Effiom</h4>
+
+              <p>LSQFf587g90</p>
+            </div>
+          </div>
+
+          <div className={detailsStyles.tierContianer}>
+            <p>User’s Tier</p>
+
+            <div className={detailsStyles.starFlex}>
+              <FullStar />
+              <EmptyStar />
+              <EmptyStar />
+            </div>
+          </div>
+
+          <div className={detailsStyles.numeralContainer}>
+            <h4>₦200,000.00</h4>
+            <p>9912345678/Providus Bank</p>
+          </div>
         </div>
       </div>
     </div>
