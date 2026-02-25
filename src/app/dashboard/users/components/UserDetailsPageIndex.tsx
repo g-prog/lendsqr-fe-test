2c15edd36c3d7cbf6869fc20c723cc4aeb4b2cdb
Dantown <dantown@Dantowns-MacBook-Air.local>
2026-02-25 11:15:11 +0100

    making progress with top section of user details page

--- src/app/dashboard/users/components/UserDetailsPageIndex.tsx
+++ src/app/dashboard/users/components/UserDetailsPageIndex.tsx
@@ -1,5 +1,6 @@
 import detailsStyles from "../userdetails.module.scss";
 import LongForwardIcon from "@/components/icons/LongForwardIcon";
+import SvgAvatar from "@/components/icons/SvgAvatar";
 
 const UserDetailsPageIndex = () => {
   return (
@@ -10,6 +11,20 @@ const UserDetailsPageIndex = () => {
         </div>
 
         <p>Back to Users</p>
+      </div>
+
+      <div className={detailsStyles.topFlex}>
+        <h4 className={detailsStyles.userHeader}>User Details</h4>
+        <div className={detailsStyles.topRight}>
+          <div className={detailsStyles.blackListBtn}>Blacklist User</div>
+          <div className={detailsStyles.activateBtn}>Activate User</div>
+        </div>
+      </div>
+
+      <div className={detailsStyles.generalInfoDiv}>
+        <div className={detailsStyles.iconContainer}>
+          <SvgAvatar />
+        </div>
       </div>
     </div>
   );
