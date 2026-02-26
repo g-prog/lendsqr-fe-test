e9c861f9ba51e5cbb6b6d172b94f586d6fe81bcc
buky <alao@connou.app>
2026-02-26 11:55:13 +0100

    added tests for user details page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,25 @@
 import type { Metadata } from "next";
-import { Work_Sans,  Montserrat, Roboto, Inter   } from "next/font/google";
+import { Work_Sans, Montserrat, Roboto, Inter } from "next/font/google";
 import "./globals.scss";
 
+
 const workSans = Work_Sans({
-  subsets: ['latin'],
-  weight: ['400', '500', '600', '700'],
-  variable: '--font-work-sans'
-})
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-work-sans",
+});
 
 const inter = Inter({
   subsets: ["latin"],
-   weight: ['400', '500', '600', '700'],
+  weight: ["400", "500", "600", "700"],
   variable: "--font-inter",
 });
 
 const roboto = Roboto({
-  subsets: ['latin'],
-  weight: ['400', '500', '700'], 
-  variable: '--font-roboto', 
-})
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
+  variable: "--font-roboto",
+});
 
 const montserrat = Montserrat({
   subsets: ["latin"],
@@ -37,7 +38,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${workSans.variable} ${montserrat.variable} ${roboto.variable} ${inter.variable}`}>
+      <body
+        className={`${workSans.variable} ${montserrat.variable} ${roboto.variable} ${inter.variable}`}
+      >
         {children}
       </body>
     </html>
